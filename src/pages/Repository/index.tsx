5dbf7c6e046e9c724c1144524317bbe51a3b978a
Matheus Moreira Ferreira <matheusmoreira.g6@gmail.com>
2021-03-14 17:13:02 -0300

    fix: navigation between pages

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router-dom';
+
+interface RepositoryParamns {
+  repository: string;
+}
 
 const Repository: React.FC = () => {
-  return <h1>Repository</h1>;
+  const { params } = useRouteMatch<RepositoryParamns>();
+  return <h1>Repository: {params.repository}</h1>;
 };
 
 export default Repository;
